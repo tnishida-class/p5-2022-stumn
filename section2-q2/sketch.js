a0e93558d2c713ca72b52ab56fd020c42323b0ad
stumn <pukut0317@gmail.com>
2022-10-11 22:56:49 +0900

    Update sec2-2q

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -1,13 +1,38 @@
 // チェッカー
 function setup() {
   createCanvas(200, 200);
-  background(196);
   let size = width / 8;
   noStroke();
+  // 格子模様
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      rect(i*25,j*25,10,10);
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      if((i+j)% 2 == 1){
+        fill(100);
+      }
+      else{
+        fill(256);
+      }
+      rect(i*8,j*8,8,8);
+    }
+  }
+  for(let c=0; c<8; c++){
+    for(let d=0; d<8; d++){
+      if((c+d)%2 == 1){
+        if(d<3){
+          fill(256,0,0);  
+        }
+        if(d>4){
+          fill(0);
+        }
+        if((d==3)||(d==4)){
+          noFill();
+        }
+      } 
+      else{
+        noFill();
+      }
+     
+      ellipse(c*8+4,d*8+4,7);
     }
   }
 }

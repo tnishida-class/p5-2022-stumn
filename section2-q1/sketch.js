6f3bc3afec8fdcbcde1f92b5ad0ea0fbb07307b1
stumn <pukut0317@gmail.com>
2022-10-11 11:45:18 +0900

    Change sec2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,13 @@
-// 小手調べ
 function setup(){
-  for(let i = 0; i < 10; i++){
-    // BLANK[1]
+  for(let i = 1; i <= 10; i++){
+    noFill();
+    strokeWeight(1);
+    if(i<=5){
+      stroke(0,0,255)
+    }
+    else{
+      stroke(255,0,0)
+    }
+    ellipse(50,50,i*10);
   }
 }
--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -1,10 +1,12 @@
 // チェッカー
 function setup() {
   createCanvas(200, 200);
+  background(196);
   let size = width / 8;
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
+      rect(i*25,j*25,10,10);
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
     }
   }

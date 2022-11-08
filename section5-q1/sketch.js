cf869d1a4a9db28a73f931828df466c9ab6e4e7d
stumn <pukut0317@gmail.com>
2022-11-08 12:05:35 +0900

    Update sec5-q1 hukidashi

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -4,15 +4,16 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love keyakizaka46",32,15);
 }
 
-function balloon(t){
+function balloon(t,S,p){
+  textSize(S);
   let w = textWidth(t);
   let h = textAscent() + textDescent();
-  let p = 2;
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
-  fill(255);
-  text(t, p, h + p);
+  fill(61, 48, 74); 
+  triangle(50+w/3,50+h+p*2,50+w/4,80+h+p*2,50+3*w/4,50+h+p*2);
+  rect(50, 50, w + p * 2, h + p * 2,15);
+  fill(82, 171, 85);
+  text(t, p+50, h+p+50); 
 }

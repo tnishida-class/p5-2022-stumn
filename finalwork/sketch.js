c8b7bd216f2668192900ffed2f090461b448c731
stumn <pukut0317@gmail.com>
2022-11-15 12:13:29 +0900

    Update final-work
    幾何学模様を描画する
    出来れば、円形に砂を振り回すやつにしたい。

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -2,12 +2,48 @@
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
-}
-
-function draw(){
+  count = 0;
+  cycle = 100;
+  x = width / 2;
+  y = height / 2;
+  vx = 2;
+  vy = 2;
+  noStroke();
   background(160, 192, 255);
 }
 
 function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
 }
+
+let A, x, y, z, vx, vy;
+const vyMax = 30;
+
+function mouseClicked(){
+  A = mouseX
+  z = mouseY;
+}
+
+function draw(){
+  x += vx;
+  y += vy;
+  vy = constrain(vy, -vyMax, vyMax);
+
+  if(x < 0 || x > width){ vx = -1 * vx; }
+  if(y < 0 || y > height){ vy = -1 * vy; }
+  x = constrain(x, 0, width);
+  y = constrain(y, 0, height);
+
+  if(keyIsDown(LEFT_ARROW)){ x = x-5 }
+  if(keyIsDown(RIGHT_ARROW)){ x = x+5 }
+  if(keyIsDown(UP_ARROW)){ y = y-5 }
+  if(keyIsDown(DOWN_ARROW)){ y = y+5 }
+  ellipse(x, y, 20, 20);
+
+  let size = count;
+  let R = random(0,256);  
+  count = (count+1) % cycle;
+  fill(z,A,R);
+  ellipse(A,z,size);
+}
+

a8016d3f4179c0beebe80d367a3ab16f3c29acc0
stumn <pukut0317@gmail.com>
2022-11-26 19:09:01 +0900

    sec3-q1 コメント後の変更

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -12,14 +12,12 @@ function draw(){
   background(160, 192, 255);  
   let size = count ;  
   count = (count+1) % cycle ;
-  // if(keyIsDown(UP_ARROW)){ count = (count + 3 ) % cycle; }
-  // if(keyIsDown(DOWN_ARROW)){ count = (count - 3 ) % cycle; } 
-
-  for(let x = 120; x < 256; x=x+50){
-    fill(x,72,50);
-    noStroke();
-    ellipse(width / 2, height / 2, size );
-  }
+  if(keyIsDown(UP_ARROW)){ count = (count + 3 ) % cycle; }
+  if(keyIsDown(DOWN_ARROW)){ count = (count - 3 ) % cycle; } 
+  let green = 100 - count;
+  fill(252,green,3);
+  noStroke();
+  ellipse(width / 2, height / 2, size );
 // サイズが大きくなるにつれて、血流量が増えると思ったので、色が赤くなっていくように書いてみました。
 } 
 
